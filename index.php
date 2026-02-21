d38fe91bdbafde2f33be606cd17b858d6cc9afe5
Vincenzo Rosa <vincenzo.rosa99@gmail.com>
2026-02-21 22:40:47 +0100

    selfwork php5

--- index.php
+++ index.php
@@ -16,3 +16,35 @@ foreach ($users as $user){
         echo "Buongiorno" . $user['name'] ." ".  $user['surname'] ."\n";
     }
     }
+
+
+
+
+$numbers = [19, 28, 6, 42, 33, 65, 34,  20, 99, 10];
+
+$counter = 0;
+$total = 0;
+
+foreach($numbers as $number){
+    if ($number %2 == 0){
+        $total += $number;
+        $counter++;
+    }
+}
+echo $total/$counter;
+
+for ($i = 1; $i <= 100; $i++)
+    switch ($i) {
+        case $i %3 == 0:
+            echo "PHP \n";
+            break;
+            case $i %5 == 0:
+                echo "JAVASCRIPT \n";
+                break;
+                case $i %15 == 0:
+                echo "HACKADEMY \n";
+                break;
+                default: 
+                echo "$i \n";
+
+    };
